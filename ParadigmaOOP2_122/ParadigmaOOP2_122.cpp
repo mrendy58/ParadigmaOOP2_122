d2ab1b8addcecd0c987fae6f206e8258fdc0c33a
mrendy58 <derendyyy58@gmail.com>
2023-05-29 10:47:44 +0700

    membuat turunan pelajar dan pekerja

--- ParadigmaOOP2_122/ParadigmaOOP2_122.cpp
+++ ParadigmaOOP2_122/ParadigmaOOP2_122.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 using namespace std;
 
+class orang {
+public:
+    int umur;
+
+    orang(int pUmur) :
+        umur(pUmur)
+    {
+        cout << "orang dibuat dengan umur " << umur << "\n" << endl;
+    }
+};
+
+class pekerja : public orang {
+public:
+
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public:
+
+    pelajar(int pUmur):
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
