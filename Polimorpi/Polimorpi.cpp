5f068b7725f8440c2d59a8176edb583f67e2d78b
mrendy58 <derendyyy58@gmail.com>
2023-05-29 11:20:30 +0700

    membuat turunan joko

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -1,7 +1,22 @@
-// Polimorpi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+
+class seseorang {
+    public:
+    virtual void pesan() = 0;
+    //virtual void pesan(){
+    //  cout << "pesan dari seseorang" << endl;
+    //}
+};
+
+class joko :public seseorang {
+public:
+    void pesan() {
+        cout << "pesan dari joko" << endl;
+    }
+};
+
+
 
 int main()
 {
