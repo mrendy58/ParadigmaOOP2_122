7d825151e2e1ba2a9a4ee48da0c9a1f4ab69e346
mrendy58 <derendyyy58@gmail.com>
2023-05-29 11:10:00 +0700

    membuat turunan pelajar

--- solveMultiWaris/solveMultiWaris.cpp
+++ solveMultiWaris/solveMultiWaris.cpp
@@ -22,7 +22,15 @@ public:
     }
 };
 
+class pelajar : virtual public orang {
+public:
 
+    pelajar(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pelajar dibuat\n" << endl;
+    }
+};
 
 int main()
 {
